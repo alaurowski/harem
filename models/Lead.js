b2bcd9ba90dc254388df32896e88b30252c5eb48
Piotr Karwatka <pkarwatka@divante.pl>
2015-03-13 19:55:37 +0100

    Dodane wstępne wersje modeli

---
+++ models/Lead.js
@@ -0,0 +1,29 @@
+/**
+ * Created by pkarwatka on 13.03.15.
+ */
+var mongoose = require('mongoose');
+
+var leadSchema = new mongoose.Schema({
+
+    createdAt: Date,
+    updatedAt: Date,
+
+    title: String, /** could be name of contract */
+    subtitle: String, /** Stanowisko */
+
+    state: String,
+    stateHistory: Array,
+
+    tags: Array,
+    attachments: Array,
+
+    source: String,
+    contact: { type: mongoose.Schema.ObjectId, ref: 'contactSchema'},
+
+    owner: String
+});
+
+
+
+
+module.exports = mongoose.model('Lead', leadSchema);
