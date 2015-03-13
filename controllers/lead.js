50bbe3aee44bad2057bb8550e3a96309b414cefd
Piotr Karwatka <pkarwatka@divante.pl>
2015-03-13 19:17:55 +0100

    Added autoloader for controllers (not only for controllers)

---
+++ controllers/lead.js
@@ -0,0 +1,15 @@
+/**
+ * Created by pkarwatka on 13.03.15.
+ */
+
+module.exports = function(app){
+
+
+    app.get('/lead/index',function(req,res){
+        res.send('/ called successfully...');
+    });
+
+
+
+
+}
