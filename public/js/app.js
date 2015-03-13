8bb033cd4a3d0824f7f4eb2eac2b02f722ac0c02
Marcin Dyguda <marcin.dyguda@gmail.com>
2015-03-13 22:32:56 +0100

    Binding form data with leads API

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@
 
     }]);
 
-    app.controller('MainCtrl', ['$scope',  function($scope){
+    app.controller('MainCtrl', ['$scope', '$http',  function($scope, $http){
 
         $scope.message = 'sample message';
 
@@ -68,6 +68,35 @@
             },
         ]
 
+
+        // create a blank object to hold our form information
+        // $scope will allow this to pass between controller and view
+        $scope.formData = {};
+
+        // process the form
+        $scope.processForm = function() {
+            $http({
+                method  : 'POST',
+                url     : 'lead/edit',
+                data    : $.param($scope.formData),  // pass in data as strings
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+            })
+                .success(function(data) {
+                    console.log(data);
+
+                    if (!data.success) {
+                        // if not successful, bind errors to error variables
+                        //$scope.errorName = data.errors.name;
+                        //$scope.errorSuperhero = data.errors.superheroAlias;
+                    } else {
+                        // if successful, bind success message to message
+                        $scope.message = data.message;
+                    }
+                });
+        };
+
+
     }]);
+
 
 })();
